cd2aa14e834f8640280f3993b899b00ffa362173
Kaihang An <khangan@thoughtworks.com>
2015-12-06 03:50:16 +0800

    use selenium firefox automate login nike.com

--- ZTMDSBT.Purchase/MainWindow.xaml.cs
+++ ZTMDSBT.Purchase/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +15,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.Extensions;
+using SeleniumWebdriverHelpers;
 
 namespace ZTMDSBT.Purchase
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void button_Click(object sender, RoutedEventArgs e)
+    {
+      IWebDriver driver = new FirefoxDriver() { Url = "http://www.nike.com/cn/zh_cn/" };
+
+      driver.Navigate();
+      //      driver.FindElement(By.ClassName("exp-join-login")).Click();
+      //      driver.FindElement(By.ClassName("login-text")).Click();
+      //      driver.FindElement(By.ClassName("exp-join-login")).FindElement(By.CssSelector("a")).Click();
+      driver.ExecuteJavaScript<String>("$('.login-text').click()");
+//      driver.ExecuteJavaScript<String>("$('.modal-mask-class').show()");
+//      driver.ExecuteJavaScript<String>("$('.modal-window-class').eq(1).show()");
+      var emailInput = driver.FindElement(By.Id("exp-login-email_modal"));
+
+      emailInput.SendKeys("278325177@qq.com");
+
+      var pwdInput = driver.FindElement(By.Id("exp-login-password_modal"));
+      pwdInput.Clear();
+      pwdInput.SendKeys("Qwer0987");
+      var submit = driver.FindElement(By.ClassName("exp-login-submit"));
+      submit.Click();
+      // use wait for ajax replace thread sleep.
+      driver.WaitForAjax();
+      Screenshot screen = driver.TakeScreenshot();
+      screen.SaveAsFile("./baidu_search2.png", ImageFormat.Png);
+      MessageBox.Show("ok!");
+      //      driver.Quit();
+    }
+  }
 }
