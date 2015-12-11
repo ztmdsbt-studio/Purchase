b59029cd3a645ec05af352aed7abf82453df5aa7
Kaihang An <khangan@thoughtworks.com>
2015-12-12 02:22:09 +0800

    finished add to cart function.
    Former-commit-id: 01fca2428215d2723631d6de8e7e87d14c43d77b

--- ZTMDSBT.Purchase/MainWindow.xaml.cs
+++ ZTMDSBT.Purchase/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 
 namespace ZTMDSBT.Purchase
 {
@@ -17,13 +18,24 @@ namespace ZTMDSBT.Purchase
 
     private void button_Click(object sender, RoutedEventArgs e)
     {
-      IWebDriver driver = new FirefoxDriver() { Url = "http://www.nike.com/cn/zh_cn/" };
+
+      FirefoxProfile fp = new FirefoxProfile();
+      fp.SetPreference("webdriver.load.strategy", "fast");
+      fp.SetPreference("browser.startup.homepage", "about:blank");
+      fp.SetPreference("startup.homepage_welcome_url", "about:blank");
+      fp.SetPreference("startup.homepage_welcome_url.additional", "about:blank");
+      fp.SetPreference("browser.cache.disk.enable", "true");
+      fp.SetPreference("browser.cache.memory.enable", "true");
+      fp.SetPreference("browser.cache.offline.enable", "true");
+      fp.SetPreference("network.http.use-cache", "true");
+      IWebDriver driver = new FirefoxDriver(fp) { Url = "http://www.nike.com/cn/zh_cn/" };
       var steps = new SeleniumSteps(driver);
       var product = PurchaseConfiguration.GetProduct();
       var user = PurchaseConfiguration.GetUser();
       Task.Run(() => steps.Login(user))
         .ContinueWith(s => steps.GotoProductPage(product.URL))
-        .ContinueWith(s => steps.SelectSizeAndQuantity());
+        .ContinueWith(s => steps.SelectSizeAndQuantity(product))
+        .ContinueWith(s => steps.AddedToCart());
       //      setps.AddedToCart();
       //      setps.CreateOrder();
       //      setps.FillPaymentGetwayAndOtherInfo();
