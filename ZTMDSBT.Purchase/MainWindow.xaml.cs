be2a14fecdf46991382f2f7624c7ef657a971bf1
Kaihang An <khangan@thoughtworks.com>
2015-12-11 23:35:33 +0800

    finished login and refactor.
    Former-commit-id: 13f8835b32d6c00b2eb19e059355c02ff3b253b7

--- ZTMDSBT.Purchase/MainWindow.xaml.cs
+++ ZTMDSBT.Purchase/MainWindow.xaml.cs
@@ -1,28 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.PhantomJS;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.Extensions;
-using SeleniumWebdriverHelpers;
 
 namespace ZTMDSBT.Purchase
 {
@@ -39,29 +18,15 @@ namespace ZTMDSBT.Purchase
     private void button_Click(object sender, RoutedEventArgs e)
     {
       IWebDriver driver = new FirefoxDriver() { Url = "http://www.nike.com/cn/zh_cn/" };
-
-      driver.Navigate();
-      //      driver.FindElement(By.ClassName("exp-join-login")).Click();
-      //      driver.FindElement(By.ClassName("login-text")).Click();
-      //      driver.FindElement(By.ClassName("exp-join-login")).FindElement(By.CssSelector("a")).Click();
-      driver.ExecuteJavaScript<String>("$('.login-text').click()");
-//      driver.ExecuteJavaScript<String>("$('.modal-mask-class').show()");
-//      driver.ExecuteJavaScript<String>("$('.modal-window-class').eq(1).show()");
-      var emailInput = driver.FindElement(By.Id("exp-login-email_modal"));
-
-      emailInput.SendKeys("278325177@qq.com");
-
-      var pwdInput = driver.FindElement(By.Id("exp-login-password_modal"));
-      pwdInput.Clear();
-      pwdInput.SendKeys("Qwer0987");
-      var submit = driver.FindElement(By.ClassName("exp-login-submit"));
-      submit.Click();
-      // use wait for ajax replace thread sleep.
-      driver.WaitForAjax();
-      Screenshot screen = driver.TakeScreenshot();
-      screen.SaveAsFile("./baidu_search2.png", ImageFormat.Png);
-      MessageBox.Show("ok!");
-      //      driver.Quit();
+      var steps = new SeleniumSteps(driver);
+      var product = PurchaseConfiguration.GetProduct();
+      var user = PurchaseConfiguration.GetUser();
+      Task.Run(() => steps.Login(user))
+        .ContinueWith(s => steps.GotoProductPage(product.URL))
+        .ContinueWith(s => steps.SelectSizeAndQuantity());
+      //      setps.AddedToCart();
+      //      setps.CreateOrder();
+      //      setps.FillPaymentGetwayAndOtherInfo();
     }
   }
 }
---
+++ ZTMDSBT.Purchase/SeleniumSteps.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading;
+using System.Windows;
+using CsQuery.ExtensionMethods;
+using CsQuery.ExtensionMethods.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using SeleniumWebdriverHelpers;
+
+namespace ZTMDSBT.Purchase
+{
+  internal class SeleniumSteps
+  {
+    private IWebDriver _driver;
+
+    public SeleniumSteps(IWebDriver driver)
+    {
+      _driver = driver;
+    }
+
+    public SeleniumSteps Login(User user)
+    {
+      _driver.Navigate();
+      _driver.ExecuteJavaScript<string>("$('.login-text').click()");
+      var emailInput = _driver.FindElement(By.Id("exp-login-email_modal"));
+
+      emailInput.SendKeys(user.Username);
+
+      var pwdInput = _driver.FindElement(By.Id("exp-login-password_modal"));
+      pwdInput.Clear();
+      pwdInput.SendKeys(user.Password);
+      var submit = _driver.FindElement(By.ClassName("exp-login-submit"));
+      submit.Click();
+      _driver.WaitForAjax();
+      return this;
+    }
+
+    public SeleniumSteps GotoProductPage(string url)
+    {
+      _driver.Url = url;
+      _driver.Navigate();
+      return this;
+    }
+
+    public SeleniumSteps SelectSizeAndQuantity()
+    {
+      var selectContainer = _driver.FindElements(By.ClassName("exp-pdp-size-container")).FirstOrDefault(e => e.Displayed);
+      selectContainer.Click();
+      var sizeSelector = selectContainer.FindElement(By.TagName("ul"));
+      Thread.Sleep(200);
+      var option =
+        sizeSelector.FindElements(By.TagName("option"))
+          .FirstOrDefault(e => e.GetAttribute("data-label") == "(" + 46 + ")"
+                            && !e.GetAttribute("class").Contains("exp-pdp-size-not-in-stock")
+                            && e.Displayed);
+      if (option == null)
+      {
+        MessageBox.Show("out of stock.");
+        return null;
+      }
+      option.Click();
+      return this;
+    }
+
+    public SeleniumSteps AddedToCart()
+    {
+      throw new NotImplementedException();
+    }
+
+    public SeleniumSteps CreateOrder()
+    {
+      throw new NotImplementedException();
+    }
+
+    public SeleniumSteps FillPaymentGetwayAndOtherInfo()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
