237930ef9631fcdb5e46ba9325c8e7468be19d9c
Kaihang An <khangan@thoughtworks.com>
2015-12-11 23:35:33 +0800

    finished login and refactor.

---
+++ ZTMDSBT.Purchase/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ZTMDSBT.Purchase
+{
+  public class User
+  {
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string Name { get; set; }
+  }
+}
