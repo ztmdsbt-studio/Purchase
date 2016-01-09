fe38954b05732f46f6102ce1ee80e41da72ce899
Kaihang An <khangan@thoughtworks.com>
2016-01-09 23:43:56 +0800

    add request related classes.

---
+++ ZTMDSBT.Purchase/Service/CookiesManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTMDSBT.Purchase.Service
+{
+  public class CookiesManager
+  {
+    private static readonly Lazy<CookiesManager> _current = new Lazy<CookiesManager>();
+    private Dictionary<string, CookieContainer> _containers;
+
+    private CookiesManager()
+    {
+      _containers = new Dictionary<string, CookieContainer>();
+    }
+
+    public static CookiesManager Current => _current.Value;
+
+    public CookieContainer this[string userId]
+    {
+      get { throw new NotImplementedException(); }
+      set { throw new NotImplementedException(); }
+    }
+  }
+}
