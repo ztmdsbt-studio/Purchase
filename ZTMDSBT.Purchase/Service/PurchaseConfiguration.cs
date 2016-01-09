8e71ce58adc3cf409bced590b8e28b4bdb7710fc
Kaihang An <khangan@thoughtworks.com>
2016-01-09 23:43:56 +0800

    add request related classes.
    Former-commit-id: fe38954b05732f46f6102ce1ee80e41da72ce899

---
+++ ZTMDSBT.Purchase/Service/PurchaseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using ZTMDSBT.Purchase.Models;
+using json = Newtonsoft.Json;
+
+namespace ZTMDSBT.Purchase.Service
+{
+  public static class PurchaseConfiguration
+  {
+    public static Product GetProduct(string gender = "男")
+    {
+      var productFileName = gender == "男" ? "product1.json" : "product2.json";
+      return json.JsonConvert.DeserializeObject<Product>(File.ReadAllText("./fixtures/" + productFileName));
+    }
+
+    public static User GetUser()
+    {
+      return json.JsonConvert.DeserializeObject<User>(File.ReadAllText("./fixtures/user.json"));
+    }
+  }
+}
---
+++ ZTMDSBT.Purchase/Service/RequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using ZTMDSBT.Purchase.Models;
+
+namespace ZTMDSBT.Purchase.Service
+{
+  public class RequestHandler
+  {
+//    private static readonly Lazy<RequestHandler> _current = new Lazy<RequestHandler>(() => new RequestHandler());
+//    public static RequestHandler Current => _current.Value;
+    private HttpClient _client;
+
+    public RequestHandler()
+    {
+      _client = new HttpClient();
+    }
+  }
+}
