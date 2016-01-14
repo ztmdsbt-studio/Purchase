f654fb1c4cceba008d575bb069690aa637ce0e04
Kaihang An <khangan@thoughtworks.com>
2016-01-15 03:11:53 +0800

    refactor, use HttpClient to send request. finish login request. find a way to store cookie.
    Former-commit-id: 73b64850c752ea1184143467d4b720b7776eebe1

---
+++ ZTMDSBT.Purchase/Service/PuchaseRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZTMDSBT.Purchase.Service
+{
+  public class PurchaseHttpHandler : HttpClientHandler
+  {
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+      request.Headers.Referrer = new Uri("http://store.nike.com/cn/zh_cn/");
+      request.Headers.Add("UserAgent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
+      request.Headers.Add("Origin", "http://www.nike.com");
+//      request.Headers.Add("Accept", "application/json, text/javascript, */*; q=0.01");
+//      request.Headers.Add("Content-Locale", "zh_CN");
+//      request.Headers.Add("Authorization", "CPC");
+//      request.Headers.Add("DNT", "1");
+//      request.Headers.Add("Accept-Encoding", "gzip, deflate");
+//      request.Headers.Add("Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2");
+      return base.SendAsync(request, cancellationToken);
+    }
+  }
+}
